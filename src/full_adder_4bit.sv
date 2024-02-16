4cda5e5220bc915e86c1100aaf0e3c5de83937b3
esteban marin <esmarin@gmail.com>
2024-02-15 20:15:47 -0800

    fix bug

--- src/full_adder_4bit.sv
+++ src/full_adder_4bit.sv
@@ -17,7 +17,7 @@ module tt_um_esmarin (
     input  wire       rst_n     // reset_n - low to reset
 );
     // use bidirectional pins 4 for output(1) and 4  for input (0)
-    assign uio_oe = 8'11110000;
+    assign uio_oe = 11110000;
 
     // internal signals used in this module
     logic c1, c2, c3;
@@ -26,12 +26,12 @@ module tt_um_esmarin (
     full_adder FA0(ui_in[0], ui_in[4], uio_in[0], uo_out[0], c1);
     full_adder FA1(ui_in[1], ui_in[5], c1,        uo_out[1], c2);
     full_adder FA2(ui_in[2], ui_in[6], c2,        uo_out[2], c3);
-    full_adder FA3(ui_in[3], ui_in[7], c3,        ou_out[3], ou_out[4]);
+    full_adder FA3(ui_in[3], ui_in[7], c3,        uo_out[3], uo_out[4]);
 
     // All output pins must be assigned. If not used, assign to 0.
     assign uo_out [7:5] = 0;
     assign uio_out[7:4] = 0;
-    assign uio_en [7:4] = 0;
+    assign uio_oe [7:4] = 0;
 
 
 endmodule
